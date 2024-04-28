b75b7993cf8fd5d10b7f4dd5a9ecae4634f647a3
Henrik Wolf <henrik-wolf@freenet.de>
2024-04-28 23:08:29 +0200

    include loading of cities

--- src/CoolWalks.jl
+++ src/CoolWalks.jl
@@ -5,4 +5,12 @@ using Reexport
 
 @reexport using DataFrames
 
+include("city_types.jl")
+export SB_City
+include("city_setup_types.jl")
+export RealCitySetup, HexagonCitySetup, RectangleCitySetup, RandomCitySetup
+
+include("city_loading.jl")
+export load_city
+
 end

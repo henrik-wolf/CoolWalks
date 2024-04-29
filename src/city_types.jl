0dbf3b792e9913469384fc0bdd05db262102809c
Henrik Wolf <henrik-wolf@freenet.de>
2024-04-29 14:19:11 +0200

    added observatory to city type

--- src/city_types.jl
+++ src/city_types.jl
@@ -1,18 +1,21 @@
 abstract type AbstractCityDataset end
 mutable struct SB_City <: AbstractCityDataset
     name::Symbol
+    observatory::ShadowObservatory
     streets::MetaGraphs.MetaDiGraph
     buildings::DataFrame
     shadows::DataFrame
 end
 mutable struct ST_City <: AbstractCityDataset
     name::Symbol
+    observatory::ShadowObservatory
     streets::MetaGraphs.MetaDiGraph
     trees::DataFrame
     shadows::DataFrame
 end
 mutable struct SBT_City <: AbstractCityDataset
     name::Symbol
+    observatory::ShadowObservatory
     streets::MetaGraphs.MetaDiGraph
     buildings::DataFrame
     trees::DataFrame

72b4ca4a954187732a4ba3f420acec968dec501f
Henrik Wolf <henrik-wolf@freenet.de>
2024-04-29 14:20:00 +0200

    include everything

--- src/CoolWalks.jl
+++ src/CoolWalks.jl
@@ -2,15 +2,37 @@ module CoolWalks
 
 using DrWatson
 using Reexport
+using Graphs, MetaGraphs
+using TimeZones
+using ShadowGraphs
+using CompositeBuildings
+using GeoInterfaceMakie
+using ArchGDAL
+using CoolWalksUtils
+using MinistryOfCoolWalks
+using Random
+using Distributions
+using GeometryBasics
+using VoronoiCells
+using SpatialIndexing
+using GeoInterface
 
 @reexport using DataFrames
 
+GeoInterfaceMakie.@enable ArchGDAL.IGeometry
+
 include("city_types.jl")
 export SB_City
+
+include("overloads.jl")
+
 include("city_setup_types.jl")
 export RealCitySetup, HexagonCitySetup, RectangleCitySetup, RandomCitySetup
 
+include("synthetic_cities.jl")
+
 include("city_loading.jl")
+export MANHATTAN_CENTER, BARCELONA_CENTER, VALENCIA_CENTER
 export load_city
 
 end

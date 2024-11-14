e0b6b8a744524fb5debefc4eb9cd6169b3c6ac75
Henrik Wolf <henrik-wolf@freenet.de>
2024-11-14 13:03:17 +0100

    only run real cities

--- scripts/1_run_analysis.jl
+++ scripts/1_run_analysis.jl
@@ -3,62 +3,74 @@ using DrWatson
 using MinistryOfCoolWalks
 
 experiment = FullExperiment(ShadowWeights, SUMMER_SOLSTICE, AS)
+constant_heights = Dict(:manhattan => 40.0, :barcelona => 18.5, :valencia => 16.7)
+
+function run_everything(city_setups, outdir)
+    for constant_height in [false, true]
+        for city_setup in city_setups
+            city = load_city(city_setup)
+            if constant_height
+                set_constant_building_height!(city, constant_heights[city_setup.name])
+            end
+            result = run_experiment_on(city, experiment)
+            observatory = city.observatory
+            height = constant_height ? constant_heights[city_setup.name] : missing
+
+            out_data = @strdict city_setup experiment constant_height observatory result height
+            safesave(joinpath(outdir, "$(city.name).jld2"), out_data)
+        end
+    end
+end
 
 # MARK: Real cities, real and constant height
-let OUTDIR = datadir("exp_pro", "real_cities")
-    real_city_setups = [MANHATTAN_BIKE, MANHATTAN_WALK, BARCELONA_BIKE, BARCELONA_WALK, VALENCIA_BIKE, VALENCIA_WALK]
-    for city_setup in real_city_setups
-        for constant_height in [true, false]
-            city = load_city(city_setup)
-            if constant_height && city_setup.name == :manhattan
-                set_constant_building_height!(city, 71.0)
-            elseif city_setup.name in [:barcelona, :valencia]
-                set_constant_building_height!(city, 20.0)
-            end
-            result = run_experiment_on(city, experiment)
-            observatory = city.observatory
-
-            out_data = @strdict city_setup experiment constant_height observatory result
-            safesave(joinpath(OUTDIR, "$(city.name).jld2"), out_data)
-        end
-    end
+let OUTDIR = datadir("exp_pro", "real_cities_large", "buildings")
+    real_city_setups = [MANHATTAN_BIKE, BARCELONA_BIKE, VALENCIA_BIKE, MANHATTAN_WALK, BARCELONA_WALK, VALENCIA_WALK]
+    run_everything(real_city_setups, OUTDIR)
 end
 
-# MARK: Manhattan synthetic cities
-let OUTDIR = datadir("exp_pro", "synthetic_manhattan")
-    synthetic_city_setups = [MANHATTAN_GRID, MANHATTAN_RANDOM]
-
-    empirical_height_hist = height_distribution(MANHATTAN_BIKE)
-    for city_setup in synthetic_city_setups
-        for constant_height in [true, false]
-            city = load_city(city_setup)
-            if !constant_height
-                resample_heights!(city, empirical_height_hist)
-            end
-            result = run_experiment_on(city, experiment)
-            observatory = city.observatory
-
-            out_data = @strdict city_setup experiment constant_height observatory result
-            safesave(joinpath(OUTDIR, "$(city.name).jld2"), out_data)
-        end
-    end
+let OUTDIR = datadir("exp_pro", "real_cities_large", "parks")
+    real_city_setups = [MANHATTAN_PARK_BIKE, BARCELONA_PARK_BIKE, VALENCIA_PARK_BIKE, MANHATTAN_PARK_WALK, BARCELONA_PARK_WALK, VALENCIA_PARK_WALK]
+    run_everything(real_city_setups, OUTDIR)
 end
 
-# MARK: Barcelona synthetic cities
-let OUTDIR = datadir("exp_pro", "synthetic_barcelona")
-    empirical_height_hist = height_distribution(BARCELONA_BIKE)
 
-    for city_setup in [BARCELONA_GRID]
-        for constant_height in [false, true]
-            city = load_city(city_setup)
-            if !constant_height
-                resample_heights!(city, empirical_height_hist)
+if false
+    # MARK: Manhattan synthetic cities
+    let OUTDIR = datadir("exp_pro", "synthetic_manhattan")
+        synthetic_city_setups = [MANHATTAN_GRID, MANHATTAN_RANDOM]
+
+        empirical_height_hist = height_distribution(MANHATTAN_BIKE)
+        for city_setup in synthetic_city_setups
+            for constant_height in [true, false]
+                city = load_city(city_setup)
+                if !constant_height
+                    resample_heights!(city, empirical_height_hist)
+                end
+                result = run_experiment_on(city, experiment)
+                observatory = city.observatory
+
+                out_data = @strdict city_setup experiment constant_height observatory result
+                safesave(joinpath(OUTDIR, "$(city.name).jld2"), out_data)
             end
-            result = run_experiment_on(city, experiment)
-            observatory = city.observatory
+        end
+    end
 
-            out_data = @strdict city_setup experiment constant_height observatory result
-            safesave(joinpath(OUTDIR, "$(city.name).jld2"), out_data)
+    # MARK: Barcelona synthetic cities
+    let OUTDIR = datadir("exp_pro", "synthetic_barcelona")
+        empirical_height_hist = height_distribution(BARCELONA_BIKE)
+
+        for city_setup in [BARCELONA_GRID]
+            for constant_height in [false, true]
+                city = load_city(city_setup)
+                if !constant_height
+                    resample_heights!(city, empirical_height_hist)
+                end
+                result = run_experiment_on(city, experiment)
+                observatory = city.observatory
+
+                out_data = @strdict city_setup experiment constant_height observatory result
+                safesave(joinpath(OUTDIR, "$(city.name).jld2"), out_data)
+            end
         end
     end
 end
